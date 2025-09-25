983e7945f3db32d567170045c4a17bb2112008db
cdilallo-beep <cdilallo@itsmeccanicabruzzo.eu>
2025-09-25 16:09:55 +0200

    Delete Esame c# REST API Autore libro directory

--- Esame c# REST API Autore libro/Esame c# REST API Autore libro/Modelli/Autore.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Esame_c__REST_API_Autore_libro.Modelli
-{
-    public class Autore
-    {
-        public int AutoreId { get; set; }
-        public string Nome { get; set; }
-        public List<Libro> Libri { get; set; } = new List<Libro>();
-    }
-}
--- Esame c# REST API Autore libro/Esame c# REST API Autore libro/Modelli/Credenziali.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Esame_c__REST_API_Autore_libro.Modelli
-{
-    public class Credenziali
-    {
-        public string? Username { get; set; }
-        public string? Password { get; set; }
-    }
-}
