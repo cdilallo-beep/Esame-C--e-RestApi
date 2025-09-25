e84720d6e6596697b619b4ed068978a2700bc0a9
cdilallo-beep <cdilallo@itsmeccanicabruzzo.eu>
2025-09-25 16:13:39 +0200

    Add files via upload

---
+++ Esame c# REST API Autore libro/Esame c# REST API Autore libro/Modelli/Scrivere.cs
@@ -0,0 +1,9 @@
+﻿namespace Esame_c__REST_API_Autore_libro.Modelli
+{
+    public class Scrivere
+    {
+        public int AutoreId { get; set; }
+        public int LibroId { get; set; }
+        public string DataScrittura { get; set; } = string.Empty;
+    }
+}
